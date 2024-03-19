dbb16350c1a5df9d3403760fec27ca9cf09d19f6
pradip2004 <pradipsadhukhan16@gmail.com>
2024-03-19 19:14:28 +0530

    add effect

--- script.js
+++ script.js
@@ -66,7 +66,9 @@ const loadingAnimation = () => {
             display: "none"
       });
 }
+
 const galleryAnimation = () => {
+
       let t1 = gsap.timeline({
             scrollTrigger: {
                   trigger: ".gallery-section",
@@ -189,7 +191,7 @@ const headingAnimation = () => {
       });
 }
 
-const lastSectionAnimation = ()=>{
+const lastSectionAnimation = () => {
       let t2 = gsap.timeline({
             scrollTrigger: {
                   trigger: ".author-section .pin-title",
@@ -199,10 +201,10 @@ const lastSectionAnimation = ()=>{
                   end: "300% 0%",
                   scrub: 3,
                   pin: true
-      
+
             }
       })
-      
+
       let t4 = gsap.timeline({
             scrollTrigger: {
                   trigger: ".initiative-section",
@@ -219,7 +221,7 @@ const lastSectionAnimation = ()=>{
             ease: "expo.inOut",
             stagger: 0.1
       });
-      
+
       let t5 = gsap.timeline({
             scrollTrigger: {
                   trigger: ".desc-section .pin-title",
@@ -229,7 +231,7 @@ const lastSectionAnimation = ()=>{
                   end: "300% 0%",
                   scrub: 3,
                   pin: true
-      
+
             }
       })
 }
@@ -237,18 +239,18 @@ const lastSectionAnimation = ()=>{
 function cursorAnimation() {
       let cursor = document.querySelector(".cursor");
       // let main = document.querySelector(".main");
-  
+
       document.addEventListener("mousemove", (e) => {
             gsap.to(cursor, {
-                duration: 0.5, // Duration of the animation
-                left: e.pageX , // Target left position
-                top: e.pageY , // Target top position
-                ease: "power2.out" // Easing function
+                  duration: 0.5, // Duration of the animation
+                  left: e.pageX, // Target left position
+                  top: e.pageY, // Target top position
+                  ease: "power2.out" // Easing function
             });
-        });
-  }
- 
-  const polygonAnimation = ()=>{
+      });
+}
+
+const polygonAnimation = () => {
       gsap.timeline({
             scrollTrigger: {
                   trigger: ".photo-animation",
@@ -258,18 +260,102 @@ function cursorAnimation() {
                   end: "top 15%",
                   scrub: 3,
             }
-        }).to('.clip', {
+      }).to('.clip', {
             duration: 2,
             keyframes: [
-              { clipPath: 'polygon(0% 0%, 100% 0%, 100% 70%, 100% 100%, 0 100%, 0 70%)' },
-              { clipPath: 'polygon(0% 0%, 100% 0%, 58% 70%, 58% 100%, 42% 100%, 42% 70%)' }
+                  { clipPath: 'polygon(0% 0%, 100% 0%, 100% 70%, 100% 100%, 0 100%, 0 70%)' },
+                  { clipPath: 'polygon(0% 0%, 100% 0%, 58% 70%, 58% 100%, 42% 100%, 42% 70%)' }
             ],
             ease: 'power1.inOut',
             yoyo: true
-          });
-      
-  }
-  
+      });
+
+}
+
+const clipPaths = [
+      "circle(10% at 57.5% 27%)",
+      "circle(10% at 33% 65%)",
+      "circle(10% at 60.5% 74%)",
+      "circle(6% at 54% 48%)",
+      "circle(7% at 27% 69%)",
+      "circle(100% at 50% 50%)"
+];
+const titles = [
+      "White woman",
+      "Charrua Indigenous",
+      "Jaguar",
+      "Montevideo Hill",
+      "Ship",
+      " "
+];
+const descriptions = [
+      {
+            text: "In the foreground, we see a woman with Caucasian features representing the homeland—a white, pure, radiant, and civilized homeland ascending triumphantly. Created in the image and likeness of a Europe that embodies everything the nation should admire and pursue for success.",
+            left: "5%",
+            top: "70%"
+
+      },
+      {
+            text: "She is seated on a rock, at a higher level than the rest of the symbolic elements. She has a fabric draped over her body with the stripes of the national flag printed on it, stained with the blood of the Charrúa people. A flag that represents the patrician project of a nation-state born from the Charrúa genocide.",
+            left: "5%",
+            top: "30%"
+      },
+      {
+            text: "In the background or second layer, we see the stereotyped figure of an indigenous person in a fallen posture, looking up at the resplendent homeland.",
+            left: "50%",
+            top: "40%"
+      },
+      {
+            text: "The woman is sitting on a jaguar skin, perhaps a rug that the indigenous person obediently placed for her to sit on. The jaguar skin represents the most dangerous and wild animal of these lands, which the indigenous person killed for and because of her.",
+            left: "5%",
+            top: "20%"
+      },
+      {
+            text: "The figure of the Montevideo Hill fortress symbolizes for us the planting of the Spanish flag over the Charrúa people who fiercely resisted the establishment of this fort, symbolizing the beginning of settler colonialism, territorial dispossession.",
+            left: "50%",
+            top: "70%"
+      },
+      {
+            text: "Finally, we see the presence of a steamship in the bay of Montevideo, the means of transportation by which immigrants arrived to colonize these lands.",
+            left: "50%",
+            top: "20%"
+      },
+      {
+            text: "",
+            position: ""
+      }
+];
+
+// GSAP timeline for clip-path animations
+const clipPathTimeline = gsap.timeline({
+      scrollTrigger: {
+            trigger: ".main-section",
+            scroller: ".main",
+            pin: true,
+            start: "top top",
+            markers: false,
+            end: "+=380%",
+            scrub: 1
+      },
+      onUpdate: function () {
+            const progress = clipPathTimeline.progress();
+            const index = Math.floor(progress * (clipPaths.length - 1));
+            const description = descriptions[index];
+            document.querySelector('.main-section-title').textContent = titles[index];
+            document.querySelector('.main-section-desc').textContent = description.text;
+            document.querySelector('.main-section-desc').style.left = description.left;
+            document.querySelector('.main-section-desc').style.top = description.top;
+      }
+});
+
+// Apply clip-path animations
+clipPaths.forEach((path, index) => {
+      clipPathTimeline.to(".img-cover img", { clipPath: path, duration: 1 }, index * 3);
+
+});
+
+
+
 
 
 
